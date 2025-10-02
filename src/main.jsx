199dab6ee52859de2e625f051f7a4e84b44eace1
nprmr <neprimerov.sergey@ya.ru>
2025-10-02 22:48:15 +0300

    [fix] swipe

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./theme.css"; // подключаем палитру
+import "./theme.css";
+import WebApp from '@twa-dev/sdk';
+
+function initTelegram() {
+    WebApp.ready();
+
+    // пробуем fullscreen
+    WebApp.requestFullscreen();
+
+    // блокируем свайп вниз (закрытие)
+    if (WebApp.disableVerticalSwipes) {
+        WebApp.disableVerticalSwipes();
+    } else if (WebApp.viewport?.lockOrientation) {
+        // fallback для старых SDK
+        WebApp.viewport.lockOrientation();
+    }
+}
+
+initTelegram();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 );
-import WebApp from '@twa-dev/sdk';
-
-WebApp.ready();  // важно вызвать в начале
-
-// Пробуем fullscreen
-if (WebApp.isExpanded) {
-    WebApp.requestFullscreen();
-}
-// блокируем закрытие свайпом
-WebApp.viewport.lockOrientation();
-
